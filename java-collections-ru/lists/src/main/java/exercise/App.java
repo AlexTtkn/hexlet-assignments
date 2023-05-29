00dff1b39e5c51a0d7b7a8e41f3dc519ac63af25
@hexlet/cli <support@hexlet.io>
2023-05-29 15:32:51 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -1,15 +1,14 @@
 package exercise;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 // BEGIN
 public class App {
     public static boolean scrabble(String word1, String word2) {
-        List<String> charsFromWord1 = new ArrayList<>();
-        for (int i = 0; i < word1.length(); i++) {
-            charsFromWord1.add(word1.substring(i, i + 1));
-        }
+        String[] spliter = word1.split("");
+        List<String> charsFromWord1 = new ArrayList<>(Arrays.asList(spliter));
         for (int i = 0; i < word2.length(); i++) {
             String lowerCase = word2.substring(i, i + 1).toLowerCase();
             if (charsFromWord1.contains(lowerCase)) {
