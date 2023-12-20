ee8cdd651d52fd97838754fabe7e75c293bb82b2
@hexlet/cli <support@hexlet.io>
2023-12-20 06:16:14 +0300

    submit java-spring-ru/beans

---
+++ java-spring-ru/beans/src/main/java/exercise/daytime/Day.java
@@ -0,0 +1,21 @@
+package exercise.daytime;
+
+import jakarta.annotation.PostConstruct;
+
+
+public class Day implements Daytime {
+    private String name = "day";
+
+
+    public String getName() {
+        return name;
+    }
+
+    // BEGIN
+    @PostConstruct
+    public void init() {
+        String message = "Bean is initialized!";
+        System.out.println(message);
+    }
+    // END
+}
